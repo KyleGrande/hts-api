0b0bd866da3f7706d95d05d164aff2851b4edb00
Kyle Grande <kyle.g.grande@gmail.com>
2024-05-12 18:27:59 -0500

    router

--- prisma/migrations/20240512170132_dev/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Listing" ALTER COLUMN "location" SET DEFAULT null;
-
--- AlterTable
-ALTER TABLE "Request" ALTER COLUMN "location" SET DEFAULT null;
---
+++ src/routes/router.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+import userRoutes from "./user.routes";
+import transactionRoutes from "./transaction.routes";
+import parkingSpotRoutes from "./spot.routes";
+import requestRoutes from "./request.routes";
+
+const router = express.Router();
+
+export default router;
+router.use("/users", userRoutes);
+router.use("/transactions", transactionRoutes);
+router.use("/parkingspots", parkingSpotRoutes);
+router.use("/requests", requestRoutes);
