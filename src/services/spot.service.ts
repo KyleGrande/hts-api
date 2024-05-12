9086e15869962a230c4e933e57262bccd9aebc3c
Kyle Grande <kyle.g.grande@gmail.com>
2024-05-12 10:40:16 -0500

    spot.util

--- src/services/spot.service.ts
+++ src/services/spot.service.ts
@@ -1,34 +1,6 @@
-import { PrismaClient, ParkingSpot, Prisma } from "@prisma/client";
+import { ParkingSpot } from "@prisma/client";
+import { prisma } from "../utils/spot.util";
 import { MyLocation, MyParkingSpot } from "../interfaces/types"; // Ensure the Location type is imported correctly
-const prisma = new PrismaClient().$extends({
-  model: {
-    parkingSpot: {
-      async create(
-        userId: number,
-        status: string,
-        available: boolean,
-        departureTime: Date,
-        cost: number,
-        type: string,
-        location: MyLocation
-      ) {
-        const locationWKT = `POINT(${location.longitude} ${location.latitude})`;
-        departureTime = new Date(departureTime); // Ensure departureTime is a valid Date object
-        const parkingSpot: MyParkingSpot[] = await prisma.$queryRaw`
-            INSERT INTO "ParkingSpot" (userid, status, available, departuretime, cost, type, location)
-            VALUES (${userId}, ${status}, ${available}, ${departureTime}, ${cost}, ${type}, ST_GeomFromText(${locationWKT}, 4326))
-            RETURNING id, userid, status, available, departuretime, cost, type, location::text as location`;
-        parkingSpot[0].cost = parseFloat(
-          parkingSpot[0].cost.toFixed(2)
-        ) as unknown as number;
-        //parkingSpot[0].cost = parkingSpot[0].cost.toFixed(2);
-        return parkingSpot[0];
-      },
-    },
-  },
-});
-
-export default prisma;
 
 export async function createParkingSpot(
   userId: number,
