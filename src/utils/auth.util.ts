615487b7f2cecb6d17b174434a259d73ae787c33
Kyle Grande <kyle.g.grande@gmail.com>
2024-05-19 09:04:47 -0500

    remove try

--- src/utils/auth.util.ts
+++ src/utils/auth.util.ts
@@ -4,28 +4,17 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 export async function hashPassword(password: string) {
-  try {
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    return hashedPassword;
-  } catch (error) {
-    console.error("Error hashing password:", error);
-    return null;
-  }
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
 }
 
 export async function comparePassword(
   password: string,
   hashedPassword: string
 ) {
-  try {
-    const match = await bcrypt.compare(password, hashedPassword);
-
-    return match;
-  } catch (error) {
-    console.error("Error comparing passwords:", error);
-    return null;
-  }
+  const match = await bcrypt.compare(password, hashedPassword);
+  return match;
 }
 
 export const verifyJWT = (token: string | string[]) => {
